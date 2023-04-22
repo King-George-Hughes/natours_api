523a73504a7b306c197d10d7dd941592cbb36116
unknown <hughesgeorgeking@gmail.com>
2023-04-22 21:31:07 +0000

    changes

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
 const app = require('./app');
 
-console.log(process.env);
+// console.log(process.env);
 
 // Server
+// const PORT = process.env.PORT || 3000;
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}...`);
