46315190e99b16ff52d718f167087e66f4d05bbd
unknown <hughesgeorgeking@gmail.com>
2023-05-14 16:09:50 +0000

    changes

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -118,7 +118,7 @@ exports.deleteTour = async (req, res) => {
 };
 
 // Aggregation
-exports.grtTourStats = async (req, res) => {
+exports.getTourStats = async (req, res) => {
   try {
     const stats = await Tour.aggregate([
       {
@@ -149,6 +149,60 @@ exports.grtTourStats = async (req, res) => {
       status: 'success',
       data: {
         tour: stats,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: error,
+    });
+  }
+};
+
+// Get Monthly Plan
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = req.params.year * 1;
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDates',
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numTourStarts: { $sum: 1 },
+          tours: { $push: '$name' },
+        },
+      },
+      {
+        $addFields: { month: '$_id' },
+      },
+      {
+        // To Hide the ID field on top
+        $project: {
+          _id: 0,
+        },
+      },
+      {
+        $sort: { numTourStarts: -1 },
+      },
+      {
+        $limit: 12,
+      },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour: plan,
       },
     });
   } catch (error) {
