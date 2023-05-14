20d5643fe02b8d58ad70ad5dd05ef570940fd8b8
unknown <hughesgeorgeking@gmail.com>
2023-05-14 15:30:39 +0000

    changes

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -116,3 +116,45 @@ exports.deleteTour = async (req, res) => {
     });
   }
 };
+
+// Aggregation
+exports.grtTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          // _id: null,
+          // _id: '$difficulty',
+          _id: { $toUpper: '$difficulty' },
+          numOfTours: { $sum: 1 },
+          numOfRatings: { $sum: '$ratingsQuantity' },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+      },
+      {
+        $sort: { avgPrice: 1 },
+      },
+      // {
+      //   $match: { _id: { $ne: 'EASY' } },
+      // },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour: stats,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: error,
+    });
+  }
+};
